ccbe3edfcb9c847c5cc7bb0d0ed835ba1e95beab
Programs for biologists <igor.deyneko@inbox.ru>
2023-12-03 23:44:54 +0100

    Delete Linux directory

--- Linux/goModulesAll.bat
+++
@@ -1,3 +0,0 @@
-./goModules200bp.bat > AllModules200_out &
-./goModules100bp.bat > AllModules100_out &
-./goModules50bp.bat  > AllModules50_out 
--- Linux/goSpeedTest.bat
+++
@@ -1,1 +0,0 @@
-./mmModules testModules.dat bsubtilis_promoters_250bp.out.bin bsubtilis_background_250bp.out.bin /window 100 FreqDiff 1.0 MinCoverY 0.5 MaxCoverN 0.25

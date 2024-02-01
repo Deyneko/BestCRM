ac974d50c98a01266ce071e61e2d99a00c2c531b
Programs for biologists <igor.deyneko@inbox.ru>
2024-02-01 13:27:52 +0100

    Delete Executables directory

--- Executables/Example.bat
+++
@@ -1,4 +0,0 @@
-mm.exe matrixTFP64.lib Breast500.seq  Breast500.out all64.prf 
-mm.exe matrixTFP64.lib BreastNot500.seq  BreastNot500.out  all64.prf 
-
-mmmodules.exe  ModulesBestSitesAll.dat  Breast500.out.bin   BreastNot500.out.bin   MinCoverY 0.75 MaxCoverN 0.5 FreqDiff 1.5 >  Breast500.CRMout    
